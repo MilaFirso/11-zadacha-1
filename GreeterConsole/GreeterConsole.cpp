18a9d3a70f04dc40f3b5d3df7ae3e6ec90780645
Ludmila F <liuska86@list.ru>
2024-07-31 20:06:46 +0300

    Добавьте файлы проекта.

---
+++ GreeterConsole/GreeterConsole.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include<string> 
+#include"Greeter.h" 
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    system("chcp 1251");
+    Greeter greeter;
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    greeter.greet(name);
+
+}
---
+++ GreeterStaticLab/Greeter.cpp
@@ -0,0 +1,9 @@
+#include<iostream> 
+#include"Greeter.h" 
+
+void Greeter::greet(std::string name)
+{
+
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+
+}
